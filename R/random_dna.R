caa2e51a8ad4f37b3331b2a9cc93ef34ef1e8a88
EmilieBruun <emegbr@dtu.dk>
2021-03-22 09:11:23 +0000

    added roxygen header to create documentation

--- R/random_dna.R
+++ R/random_dna.R
@@ -1,6 +1,15 @@
-# R/random_dna.R
+# R/dna_codons_to_aa.R
+
+#' Pooling random DNA sequence
+#' 
+#' Creates a DNA sequence of \code{"l"} number of random bases 
+#' 
+#' @return A random DNA sequence with length \code{"l"}
+
 random_dna <- function(l){
   nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
   dna = paste0(nucleotides, collapse = "")
   return(dna)
 }
+
+random_dna(3)
