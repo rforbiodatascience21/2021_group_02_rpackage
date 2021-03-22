3290a091827e74f43957830ebc314d938b53171b
AstaLaugesen <asta.mimitia@gmail.com>
2021-03-22 08:32:15 +0000

    Separating into codons

---
+++ R/mk_codons.R
@@ -0,0 +1,9 @@
+
+# R/mk_codons.R
+mk_codons <- function(dna, s = 1){
+  l = nchar(dna)
+  codons <- substring(dna,
+                      first = seq(from = s, to = l-3+1, by = 3),
+                      last = seq(from = 3+s-1, to = l, by = 3))
+  return(codons)
+}
